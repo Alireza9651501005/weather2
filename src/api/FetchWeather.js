49275339f42ae85e7421cad5df2f2b78c122ad3f
Alireza <Alireza691111@gmail.com>
2021-03-15 00:37:58 -0700

    button added

--- src/api/FetchWeather.js
+++ src/api/FetchWeather.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = 'f33a484cf794d08d0148764789aaba32';
-
+// const API_KEY2 = '7de94c6922cde97de8792f71077d0d4e'
+// const URL2 = `api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY2}`
 export const fetchWeather = async (query) => {
     const { data } = await axios.get(URL, {
         params: {
